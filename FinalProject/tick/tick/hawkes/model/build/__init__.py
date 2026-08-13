6ec7aaeba8e6eaaafbca259d445cc53b60ec8ee2
Josh Joseph <98969944+TRPL-J@users.noreply.github.com>
2026-08-13 17:25:53 -0400

    Upload the hawkes related files
    Note, all these files are for python and not cpp. I will be adding that shortly.

---
+++ FinalProject/tick/tick/hawkes/model/build/__init__.py
@@ -0,0 +1,17 @@
+# License: BSD 3 clause
+
+from tick.base.opsys import add_to_path_if_windows, load_extension, \
+    resolve_repo_root
+
+add_to_path_if_windows(__file__)
+_REPO_ROOT = resolve_repo_root(__file__, levels_up=3)
+
+
+def _load_extension(module_name):
+    module = load_extension(module_name, __name__, __file__,
+                            repo_root=_REPO_ROOT)
+    globals()[module_name] = module
+    return module
+
+
+hawkes_model = _load_extension("hawkes_model")
